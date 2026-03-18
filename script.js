82def88c345061cd2e6fc8d638e446372032b193
Darkdreamnit <pommellskevin@gmail.com>
2026-03-18 16:46:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -463,7 +463,5 @@ document.querySelectorAll('#dtcSearch, .suggestion-btn').forEach(element => {
         });
     }
 
-    div.onclick = () => {
-  window.location.href = `dtc.html?code=${dtc.code}`;
-};
+    
 });
