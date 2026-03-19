8f2bf258e7b7680bb9c31cde2105e5ca5de14659
Darkdreamnit <pommellskevin@gmail.com>
2026-03-19 15:51:23 -0400

    Update dtcData.js

--- dtcData.js
+++ dtcData.js
@@ -14351,3 +14351,15 @@ dtcDatabase["P0420"] = {
   relatedCodes: ["P0430", "P0171", "P0300"]
 };
 
+dtc.quickFacts?.forEach(item => {
+  quickFacts.innerHTML += `<div><strong>${item.label}:</strong> ${item.value}</div>`;
+});
+
+dtc.symptomTable?.forEach(row => {
+  symptomTable.innerHTML += `
+    <tr>
+      <td>${row.issue}</td>
+      <td>${row.likelyCause}</td>
+    </tr>
+  `;
+});
